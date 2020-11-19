340cb68a1228a14231997659c556f5b43e8b3216
DedaLenya <DedaLenya81@gmail.com>
2020-11-20 00:42:45 +0500

    очередной коммит из гитбаш от 20,11,2

---
+++ lession_folder/cw_19112020.py
@@ -0,0 +1,12 @@
+# print("hello world")
+workers = []
+for i in range(0,3):
+    print("index", i)
+
+    worker = {
+    "surname": "Murylev",
+    "age": 39,
+    "covid_test": "True"
+    }
+    workers.append(worker)
+print(workers)
