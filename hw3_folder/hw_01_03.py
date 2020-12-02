637932a252496d612c98bcc104c70f9d9e54e2b9
DedaLenya <DedaLenya81@gmail.com>
2020-12-02 19:53:16 +0500

    убираем вариант с try except, оставляем if(ы)

--- hw3_folder/hw_01_03.py
+++ hw3_folder/hw_01_03.py
@@ -7,19 +7,10 @@
 def foo_division(number1, number2):
     if number2 == 0:
         print("Деление на 0!!, завершаем ошибкой")
-    result = number1 / number2
-    print(f"результат деления чисел {result} = ")
+        return None
+    else:
+        result = number1 / number2
+        print(f"результат деления чисел {result} = ")
 
 
 foo_division(int(input("Делимое>> ")), int(input("Делитель>> ")))
-
-
-# def foo_division(number1, number2):
-#     try:
-#         result = number1 / number2
-#     except ZeroDivisionError as e:
-#         print("Деление на 0!!, завершаем ошибкой - ", e)
-#     print(f"результат деления чисел = {result} ")
-#
-#
-# foo_division(int(input("Делимое>> ")), int(input("Делитель>> ")))
