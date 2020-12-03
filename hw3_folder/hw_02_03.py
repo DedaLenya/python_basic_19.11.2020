4695db0a92f077864908137f4d6e6738ce6f0043
DedaLenya <DedaLenya81@gmail.com>
2020-12-03 21:48:43 +0500

    добавил для разнообразия интпуты и проверку имейла

--- hw3_folder/hw_02_03.py
+++ hw3_folder/hw_02_03.py
@@ -6,13 +6,20 @@
 # Вариант вывода в строку, через именнованые аргументы превращающиеся в словарь
 
 
-def directory(**kwargs):
+email = input('имейл>>')
+if not email.endswith("@mail.ru"):
+    print("Ошибка ввода имейла, всё упало.")
+    quit()
+name = input('введите имя>>')
+year = int(input('год рождения>>'))
+city = input('город>>')
 
+
+def directory(**kwargs):
     print(kwargs)
 
 
-directory(name="Deda", surname="Lenya", year="1981", city="Екатеринбург", email="ip66net", tel="0006")
-
+directory(name=name, year=year, city=city, email=email)
 
 # Вариант вывода читабельный, перебором словаря, через именнованые аргументы превращающиеся в словарь.
 
