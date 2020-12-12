f98463e8586fb1d68a2fae4ddf75b5c385a31e7c
DedaLenya <DedaLenya81@gmail.com>
2020-12-12 22:46:11 +0500

    HW5 commit from pycharm

---
+++ hw5_folder/hw3_5.py
@@ -0,0 +1,20 @@
+"""3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+Определить, кто из сотрудников имеет оклад менее 20 тыс.,
+вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников."""
+
+d = {}
+count = 0
+with open("example3.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        count += 1
+        key, value = line.split()
+        d[key] = int(value)
+
+
+with open("example3.txt", "r", encoding="utf-8") as file:
+    print(f"Общий список сотрудников и окладов из файла:\n{file.read()}")
+midl_prof = (sum(d.values())) / count
+print(f"Средний доход на {count} сотрудников {midl_prof}\n")
+for key, value in d.items():
+    if value < 20000:
+        print(f"Оклады менее 20 т.р. : {key}, {value}")
---
+++ hw5_folder/hw4_5.py
@@ -0,0 +1,24 @@
+"""4. Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую
+построчно данные. При этом английские числительные должны заменяться на русские.
+Новый блок строк должен записываться в новый текстовый файл.
+"""
+my_dict = {
+    "One": "Один",
+    "Two": "Два",
+    "Three": "Три",
+    "Four": "Четыре"
+}
+
+with open("example4.txt", "r", encoding="utf-8") as file:
+    with open("example4_1.txt", "w", encoding="utf-8") as final_file:
+        lines = file.readlines()
+        for line in lines:
+            num_list = line.split(' — ')
+            final_file.write(f'{my_dict[num_list[0]]} — {num_list[1]}')
+with open("example4_1.txt", "r", encoding="utf-8") as final_file:
+    print(final_file.read())
